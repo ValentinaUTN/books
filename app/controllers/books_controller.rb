e8f27f158c847730d9bfb1de29f95fe6e0ef08a6
Arisvis <fabricio_arisvis64@hotmail.com>
2017-10-28 11:08:56 -0300

    creamos la db

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,8 @@
 class BooksController < ApplicationController
+def index #definir una funcion
+@books = # ["Pragmatic Programer", 
+			#"Eloquent Ruby",
+			#"Secrets of the JavaScript Ninja"]#genera una variable de instancia 
+			Book.all
+end
 end
