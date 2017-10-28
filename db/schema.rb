35a476cb8d6230e8f2841b2d943260f129de72d1
Arisvis <fabricio_arisvis64@hotmail.com>
2017-10-28 11:40:17 -0300

    agregamos campo a la db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028142232) do
+ActiveRecord::Schema.define(version: 20171028142813) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171028142232) do
     t.date "publication_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price"
   end
 
 end
